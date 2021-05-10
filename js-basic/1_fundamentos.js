8d30514323cc5b6b2e3cd0913044a2ae70854e56
oscargon12 <oscargon12@gmail.com>
2021-05-10 00:11:53 -0500

    clase coersion

--- js-basic/1_fundamentos.js
+++ js-basic/1_fundamentos.js
@@ -53,11 +53,68 @@ const func2 = function(a,b){
 func2(4,3)
 
 function saludarEstudiantes(estudiante) {
-    console.log(`Hola ${estudiante}`)
+    console.log(`Hola ${estudiante}`);
     }
 
-saludarEstudiantes(Oscar)
+saludarEstudiantes(Oscar);
 
-const func3 = () =>{
+function sumar(a,b){
+    let resultado = a + b;
 
+    return resultado;
 }
+
+sumar(76,89);
+
+
+// === Scope ===
+/* El scope es el alcance a las variables, hay variables afuera de las funciones
+ (Scope global) y adentro de las funciones (Scope local). las funciones pueden 
+ acceder a las variables locales, pero no se puede acceder a las variables
+locales */
+
+let miNombre = 'Oscar';
+
+function nombre(){
+    let miApellido = 'Gonzalez';
+
+    console.log(miNombre + ' ' + miApellido);
+}
+
+nombre();
+
+
+// === Hoisting ===
+/* Es cuando las variables o funciones se declaran al inicio del código, 
+antes de que se ejecute cualquier línea de código */
+
+/* Para evitar que esto parseFloat, es mejor declararlas al inicio del código
+Si tengo que usar unescape, tengo que irme hasta el inicio y declararla ahí */
+
+
+// === Coersión ===
+/* Es cambiar los tipos de valores (Ejem: de number a string) */
+
+// Coersión implicita
+//Cambia el tipo de dato automáticamente
+
+let coer1 = 4 + '7';
+console.log(typeof coer1); // Imprime '47' como string
+
+// Coersión explicita
+// El cambio de tipo de dato, se hace de manera obligada
+
+let a = '20';
+let b = a + '';
+
+typeof b;
+
+let c = String(a);
+
+console.log(c);
+
+let d = Number(c);
+
+typeof d
+
+
