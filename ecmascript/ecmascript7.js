8cb2df9ad8d5704c7a1489952f3b40e44872cdfd
oscargon12 <oscargon12@gmail.com>
2021-05-18 09:36:43 -0500

    merge en ES 7

--- ecmascript/ecmascript7.js
+++ ecmascript/ecmascript7.js
@@ -1,39 +1,19 @@
-<<<<<<< HEAD
-//Includes
-console.log('== Includes ==')
-// Valida si en un array existe un determinado elemento
-=======
 //Que ha de nuevo?
 
 //Includes
 console.log('== Includes ==')
->>>>>>> f58f3cdb03268a2d50dfe50e6fb77ec750a303cc
 
 let numbers = [1,2,3,7,8];
 
 if (numbers.includes(7)){
-<<<<<<< HEAD
-    console.log(`${numbers} Si tiene ese numero`)
-} else {
-    console.log(`${numbers} no tiene ese numero`)
-=======
     console.log('Si se encuentra el valor en el arreglo numbers');
 } else {
     console.log('No se encuentra el numero');
->>>>>>> f58f3cdb03268a2d50dfe50e6fb77ec750a303cc
 }
 
 
 //Exponencias
 console.log('== Exponencias ==')
-<<<<<<< HEAD
-// Puedo elevar numeros a potencias con ***
-
-let base = 4;
-let exponent = 3;
-let result = base ** exponent;
-console.log(result);
-=======
 
 let base = 4;
 let exponent = 3;
@@ -43,4 +23,3 @@ let result = base ** exponent;
 console.log(result);
 
 
->>>>>>> f58f3cdb03268a2d50dfe50e6fb77ec750a303cc
